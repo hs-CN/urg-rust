37c8d3255a25acd3f1d3547715fed6a86dd8283c
hs-CN <hs19931202@outlook.com>
2022-12-20 11:48:08 +0800

    refactor

--- examples/get_distance_intensity_infinite.rs
+++ examples/get_distance_intensity_infinite.rs
@@ -1,4 +1,3 @@
-use urg_rust;
 fn main() {
     let mut urg = urg_rust::Urg::open("192.168.0.10".parse().unwrap(), 10940).unwrap();
     println!("start capture");
--- examples/get_version.rs
+++ examples/get_version.rs
@@ -1,4 +1,3 @@
-use urg_rust;
 fn main() {
     let urg = urg_rust::Urg::open("192.168.0.10".parse().unwrap(), 10940).unwrap();
     println!("{:?}", urg.get_version_info());
